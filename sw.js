d05e98f459d8fa26a3e371c6ee785023c51b76a1
phamminhquang206 <phamminhquang206@gmail.com>
2026-08-28 22:50:35 +0700

    Minor changes AI chat template

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables Offline Capabilities & Progressive Web App (PWA) Functionality
  */
 
-const CACHE_NAME = 'qpm-stock-v1.7.1';
+const CACHE_NAME = 'qpm-stock-v1.7.2';
 const STATIC_ASSETS = [
     './',
     './index.html',
